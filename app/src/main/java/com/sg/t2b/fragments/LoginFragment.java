91d7d85f207b06bbc67b2d8af3418914b1242319
Trung <trungnq18@fsoft.com.vn>
2020-01-06 16:45:09 +0700

    add ui

--- app/src/main/java/com/sg/t2b/fragments/LoginFragment.java
+++ app/src/main/java/com/sg/t2b/fragments/LoginFragment.java
@@ -1,4 +1,31 @@
 package com.sg.t2b.fragments;
 
-public class LoginFragment {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.sg.t2b.Navigator;
+import com.sg.t2b.R;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class LoginFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_login, container, false);
+        ButterKnife.bind(this, view);
+        return  view;
+    }
+    @OnClick(R.id.btn_login)
+    public void login()
+    {
+        Navigator.Instance().pushFragment(new SignUpFragment());
+    }
 }
--- app/src/main/java/com/sg/t2b/fragments/SignUpFragment.java
+++ app/src/main/java/com/sg/t2b/fragments/SignUpFragment.java
@@ -1,4 +1,31 @@
 package com.sg.t2b.fragments;
 
-public class SignUpFragment {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.sg.t2b.Navigator;
+import com.sg.t2b.R;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class SignUpFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_signup, container, false);
+        ButterKnife.bind(this,view);
+        return view;
+    }
+    @OnClick(R.id.btn_back)
+    public void popBack()
+    {
+        Navigator.Instance().popBack();
+    }
 }
